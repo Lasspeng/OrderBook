5d894bc9f7b5dd9b761590a617cbe057a5998c69
Gavin Liburd <GavinLiburd901@gmail.com>
2026-04-10 12:38:00 -0400

    Created aliases for use throughout the orderbook

---
+++ include/types.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <cstdint>
+
+enum class Side
+{
+  Bid,
+  Ask
+};
+
+using Price = std::uint64_t;
+using OrderId = std::uint64_t;
+using Volume = std::uint32_t;
+using SequenceNum = std::uint64_t;
